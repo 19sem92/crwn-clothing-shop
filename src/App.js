50637fbf37da4caded0f900e29075363bbfe71a4
Sergey Chirva <19sem92@gmail.com>
2019-12-08 16:45:59 +0300

    add pretier

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-        <Header />
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
+      </Switch>
       {/*<HomePage/>*/}
     </div>
   );
